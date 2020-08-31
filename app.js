f7bb6c318d1fe61afe9ea7bd7a4ce98045a17eab
Edward Stark <stark@MacBook-Pro.local>
2020-08-31 15:28:29 -0400

    Add about page
    add a seperate date module
    
    add a seperate footer nad header layout

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js')
+
+const { render } = require('ejs');
 
 const app = express();
 
 let items = ['Code'];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -11,28 +15,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  let today = new Date();
 
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  };
-
-  let day = today.toLocaleDateString('en-US', options);
+    let day = date();
   // render is used for ejs, looks in views folder
-  res.render('list', { kindOfDay: day, newListItems: items });
+  res.render('list', { listTitle: day, newListItems: items });
 });
 
+
+
 app.post('/', (req, res) => {
-  let item = req.body.newItem;
+
+    let item = req.body.newItem;
+if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+} else {
+
+  
 
   items.push(item);
 
-  console.log(item);
+  console.log(item); 
 
   res.redirect('/');
-});
+}});
+
+app.get('/work', (req,res)=>{
+    res.render('list', {listTitle: 'Work List', newListItems: workItems})
+})
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    
+    res.redirect('/');
+  });
+
+app.get('/about', (req,res)=>{
+    res.render('about');
+})
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
